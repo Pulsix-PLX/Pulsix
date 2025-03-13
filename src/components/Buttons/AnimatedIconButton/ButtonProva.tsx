561208bad639f7c24c63d01bf2db2d6a16c5d014
Pallassini <pallassinimatteo@gmail.com>
2025-03-13 15:55:27 +0100

    uhu

---
+++ src/components/Buttons/AnimatedIconButton/ButtonProva.tsx
@@ -0,0 +1,8 @@
+import './ButtonProva.scss'
+function ButtonProva() {
+  return (
+    <button class='animatedButton ml-200 mt-50'><p>Bottone</p></button>
+  )
+}
+
+export default ButtonProva
--- src/routes/(Pages)/LoginRegistration/index.tsx
+++ src/routes/(Pages)/LoginRegistration/index.tsx
@@ -1,8 +1,10 @@
+import ButtonProva from "~/components/Buttons/AnimatedIconButton/ButtonProva"
 import Home from "~/components/Buttons/AnimatedIconButton/Demo"
 
 function index() {
   return (
     <>
+    <ButtonProva></ButtonProva>
     <Home></Home>
     </>
   )
